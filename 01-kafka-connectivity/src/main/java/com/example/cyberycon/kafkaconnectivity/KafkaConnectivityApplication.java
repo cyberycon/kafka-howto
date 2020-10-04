cacc580a42da9d26a611a091f50bc21b24774740
Paul Kelly <paul_the_kelly@hotmail.com>
2020-10-04 12:57:22 +0100

    Initial revision

---
+++ 01-kafka-connectivity/src/main/java/com/example/cyberycon/kafkaconnectivity/KafkaConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyberycon.kafkaconnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConnectivityApplication.class, args);
+	}
+
+}
