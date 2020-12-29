990e4b318c2061304bbe5d9e02935ab727166f87
Paul Kelly <paul_the_kelly@hotmail.com>
2020-12-29 08:51:59 +0000

    Set test consumer to consume from earliest offset

--- 01-installation-test/src/test/java/com/example/cyberycon/kafkaconnectivity/KafkaConnectivityApplicationTests.java
+++ 01-installation-test/src/test/java/com/example/cyberycon/kafkaconnectivity/KafkaConnectivityApplicationTests.java
@@ -89,6 +89,7 @@ class KafkaConnectivityApplicationTests {
 		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); 
 		return props;
 	}
 
